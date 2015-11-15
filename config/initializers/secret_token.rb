7e76df21cc1f90c521f9192886ec1bb4a9f724f9
sebtn <tonomudo@gmail.com>
2015-11-15 17:32:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flyaudio::Application.config.secret_key_base = 'ccb9f9917504c2bb9ee9f80b3a629094eb08f2a84989a07aee2ee1c32bbc6d8c041398919f5f9fe2b26af5f3317c5ccae67cbb8a9eb4c4ae5b4d985af0469478'
